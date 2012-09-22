61782d780426569b1a59c28b1f4d6d3d22afb64c
Calebe <calebe.paula.bianchini@gmail.com>
2012-09-22 20:03:34 -0300

    Ajustando classpath

--- build.sh
+++ build.sh
@@ -1,9 +1,18 @@
 !#/bin/bash
 
+classpath=$classpath:$pwd/ant-contrib-1.0b3.jar:$pwd/org-netbeans-modules-java-j2seproject-copylibstask.jar
+export classpath
+echo $classpath
+
 for i in * ; do
   if [ -d "$i" ]; then
     cd "$i"
     ant
+
+    RET=$?
+    if [ ! "$RET" = "0" ]; then
+        exit $RET
+    fi
     cd ..
   fi
 done
