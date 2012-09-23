0bd2c9d2a67064d8808dbabec528bbeea365676e
Calebe <calebe.paula.bianchini@gmail.com>
2012-09-23 15:46:39 -0300

    Reorganizando libs (jar) de projeto.

--- build.sh
+++ build.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-CopyLibs=`pwd`/org-netbeans-modules-java-j2seproject-copylibstask.jar
+_PWD=`pwd`
+CopyLibs=$_PWD/lib/org-netbeans-modules-java-j2seproject-copylibstask.jar
 
-CLASSPATH=$CLASSPATH:`pwd`/ant-contrib-1.0b3.jar:$CopyLibs
+CLASSPATH=$CLASSPATH:$_PWD/lib/ant-contrib-1.0b3.jar:$CopyLibs
 export CLASSPATH
 echo "CLASSPATH:"$CLASSPATH
 
+echo " "
+echo "Compiling these projects:"
+echo " "
 for i in * ; do
   if [ -d "$i" ]; then
     cd "$i"
+    echo "----- $i -----"
     ant -Dlibs.CopyLibs.classpath="$CopyLibs" \
-        -Dj2ee.server.home="../Enterprise Application"
+        -Dj2ee.server.home="$_PWD/glassfish-libs"
 
     RET=$?
     if [ ! "$RET" = "0" ]; then
