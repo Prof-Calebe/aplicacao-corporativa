a56253f87b1e5a6658a3caae9567061ab19060ba
Calebe <calebe.paula.bianchini@gmail.com>
2012-09-23 16:03:30 -0300

    Ajustando o script de compilação.

--- build.sh
+++ build.sh
@@ -9,18 +9,25 @@ echo "CLASSPATH:"$CLASSPATH
 
 echo " "
 echo "Compiling these projects:"
-echo " "
 for i in * ; do
   if [ -d "$i" ]; then
     cd "$i"
+
+    if [ -f "manifest.mf" ]; then
+        TARGET=jar
+    else
+        TARGET=dist
+    fi
+
+    echo " "
     echo "----- $i -----"
     ant -Dlibs.CopyLibs.classpath="$CopyLibs" \
-        -Dj2ee.server.home="$_PWD/glassfish-libs"
-
+        -Dj2ee.server.home="$_PWD/glassfish-libs" $TARGET
     RET=$?
     if [ ! "$RET" = "0" ]; then
         exit $RET
     fi
+    echo " "
     cd ..
   fi
 done
