e23d110fdc0454f48932904d011de528b8855df0
Calebe <calebe.paula.bianchini@gmail.com>
2013-02-20 11:34:36 -0300

    Estrutura bem inicial de persistência: GUI com EJB

---
+++ Java Application/src/exemplo/controller/ContatoController.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exemplo.controller;
+
+/**
+ *
+ * @author Calebe de Paula Bianchini
+ */
+public class ContatoController {
+    
+}
